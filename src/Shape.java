3214abf5a148458b33734f51447566f5958b52a2
Aditya05-Shiv <shivhareaditya92@gmail.com>
2025-08-26 21:04:04 -0700

    Calculator.java

---
+++ src/Shape.java
@@ -0,0 +1,13 @@
+public class Shape {
+    public void Draw(double l,double b){
+        double area=l*b;
+        System.out.println(area);
+
+    }
+    public void Draw(double l){
+        double area=l*l;
+        System.out.println(area);
+    }
+
+
+}
