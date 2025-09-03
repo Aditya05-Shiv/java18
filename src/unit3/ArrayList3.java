a4f8e73ce519dcb5cb6713743cd854f172832694
Aditya05-Shiv <shivhareaditya92@gmail.com>
2025-09-02 21:03:17 -0700

    ArrayList3

---
+++ src/unit3/ArrayList3.java
@@ -0,0 +1,21 @@
+package unit3;
+import java.util.*;
+
+public class ArrayList3 {
+    public static void main(String[] args) {
+        ArrayList<String> product=new ArrayList<>();
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the no.of products: ");
+        int n=sc.nextInt();
+        for(int i=0;i<n;i++){
+            product.add(sc.next());
+        }
+        Iterator itr=product.iterator();
+        while(itr.hasNext()){
+            System.out.println(itr.next());
+        }
+        Collections.sort(product);
+        System.out.println(product);
+    }
+}
+
